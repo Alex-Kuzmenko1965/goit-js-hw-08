b1bba3f94f4d19abe119f84b7a9a5b09f868e257
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-04-04 10:13:17 +0300

    Alex

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,21 +4,33 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new VimeoPlayer(iframe);
 
-let setCurrentTime = localStorage.getItem("videoplayer-current-time");
+const setCurrentTime = () => {
+    if (localStorage.getItem("videoplayer-current-time") === null) {
+        localStorage.setItem("videoplayer-current-time", 0);
+        return localStorage.getItem("videoplayer-current-time");
+    } else {
+        localStorage.getItem("videoplayer-current-time");
+    }
+};
 console.log('currentTime_init:', localStorage.getItem("videoplayer-current-time"));
 
 const onPlay = function({ seconds }) {
     console.log(seconds);
     localStorage.setItem("videoplayer-current-time", seconds);
-    setCurrentTime = localStorage.getItem("videoplayer-current-time")
+    // setCurrentTime = localStorage.getItem("videoplayer-current-time")
     // setCurrentTime = localStorage.getItem("videoplayer-current-time");
-    console.log('currentTime_play:', setCurrentTime);  
+    // console.log('currentTime_play:', setCurrentTime);  
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 player.setCurrentTime(setCurrentTime).then(function(seconds) {
-    if (setCurrentTime === null)
-    {setCurrentTime = 0;}
-    return;    
+    if (setCurrentTime === null) {
+        setCurrentTime = 0;
+        console.log('currentTime_0:', setCurrentTime);
+    }
+    else {
+        return;
+        console.log('currentTime_set:', setCurrentTime);
+    };
 });
