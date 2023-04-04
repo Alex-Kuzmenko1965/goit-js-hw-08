4f9119ce51473eb19b531839d545d24b8352f024
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-04-04 11:23:41 +0300

    Alex

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,8 +9,7 @@ console.log('currentTime_init:', localStorage.getItem("videoplayer-current-time"
 
 const onPlay = function({ seconds }) {
     console.log(seconds);
-    localStorage.setItem("videoplayer-current-time", seconds);           
-    setCurrentTime = localStorage.getItem("videoplayer-current-time");
+    localStorage.setItem("videoplayer-current-time", seconds);
     console.log('currentTime_play:', setCurrentTime);  
 };
 
@@ -27,11 +26,11 @@ player.setCurrentTime(setCurrentTime).then(function(seconds) {
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
-            console.log('время было меньше 0 или больше, чем продолжительность видео');            
+            // console.log('время было меньше 0 или больше, чем продолжительность видео');            
             break;
 
         default:
-            console.log('произошла какая-то другая ошибка')
+            // console.log('произошла какая-то другая ошибка')
             break;
     }
 });;
